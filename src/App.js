bd40aa1c21f88b47c64c8319a2ebd9f25d78af4c
fredericiaH <elsa.heron@gmail.com>
2024-07-16 15:31:00 -0400

    💄 F01: Create LoginPage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-
-import { useState } from 'react';
 import './App.css';
+import LoginPage from './components/pages/LoginPage';
 
 function App() {
-//States
-const [name, setName] = useState("");
-//Components
-const handleChange = (e) =>{
-  e.preventDefault();
-  setName(e.target.value);
-  
-}
 
-
-const handleSubmit = (e) =>{
-  e.preventDefault();
-  alert("Bonjour "+ name +".");
-  setName("");
-
-}
-
-//Render
 return(
-  <div>
-    <h1>Bienvenue chez nous</h1>  
-    <br/>
-    <h2>Connectez-vous</h2>
-    <form action='submit'onSubmit={handleSubmit} >
-      <input value={name} onChange={handleChange} placeholder='Entrez votre prenom' required />
-      <button>Accéder à votre espace</button>
-    </form>
-  </div>
+  <LoginPage />
 )
 }
 
